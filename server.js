99806f2d178a648f6fff470e8e4f1579f5537243
Bhakti-Dhanani <bhagvatiben.wappnet@gmail.com>
2025-02-20 12:46:06 +0530

    changes in server file to check for the api is runing or not

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ app.use(cors());
 
 app.use("/api/users", userRoutes);
 
-<<<<<<< HEAD
-=======
 app.get("/", (req, res) => {
   res.send("API is running successfully ....");
 });
 
->>>>>>> authapimodule
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
